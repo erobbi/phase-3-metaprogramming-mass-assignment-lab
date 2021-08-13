41696b97367338f75fcac61fcaf368febb4ae502
erobbi <elirobbins0@gmail.com>
2021-08-13 16:58:46 -0400

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  # your code here
+
+  def initialize(attributes = nil) # creates an anbitrarily long list of attributes
+    if attributes
+      attributes.each do |k, v| # key value
+        self.class.attr_accessor(k) # adds setter and getter
+        self.send("#{k}=", v) # remember to use "" not ''
+      end
+    end
+  end
 end
